d8842d43d53dd1605c29c16e9205902e6fde5d01
GitACDreamer <AC_Dreamer@163.com>
2017-04-18 10:03:59 +0800

    add some test

--- cal.py
+++ cal.py
@@ -9,10 +9,25 @@ def main():
     print('\\n\\')
     print(r'\\n\\')
     print(r'\t')
-    print('r\t\t')
+    print(r'\t\t')
     print('''line1
 line2
 line3''')
+    print(3 > 2)
+    print(3 < 2)
+    print(5 > 3 and 3 > 1)
+    print(5 > 3 or 3 < 1)
+    print(not 1 > 2)
+    print(None)
+    # 声明变量无需指定类型，它自己会判断
+    age = 12
+    if age >= 18:
+        print('adult')
+    else:
+        print('teenager')
+    # 同一个变量可以重复赋值
+    age = 'ABC'
+    print(age)
 
 
 if __name__ == '__main__':
